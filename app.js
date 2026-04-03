75124f5adc801a6dc988cba661f2863e4b237ef0
vicolat <vicolat1@gmail.com>
2026-04-03 01:06:41 +0100

    Fix port to 8080 for Elastic Beanstalk

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello DevOps World!'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
